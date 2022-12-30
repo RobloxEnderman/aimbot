256a4bea96ef4ac0099778f4987eafbbfc1ad5fd
TheLinuxNerd <79427951+RobloxEnderman@users.noreply.github.com>
2022-12-30 12:35:18 -0800

    Create aimbot.py

---
+++ aimbot.py
@@ -0,0 +1,36 @@
+import os
+import math
+import time
+from time import sleep
+import keyboard
+import math as mth
+import base64
+import subprocess
+import sys
+import pygame
+import random
+import wikipedia
+from math import pi
+# This is an updated aimbot for the game "Fortnite" that uses the new "Aimbot" feature.
+# This code is liscnesed under the GNU General Public License v3.0
+print("Aimbot for Fortnite")
+print("By: Jaxson")
+print("Version: 1.0")
+print("This code is liscnesed under the GNU General Public License v3.0")
+#base64.encode(pi)
+#print(pi)
+print("Are you sure you would like to start aimbot? (y/n)")
+start = input()
+if start == "y":
+    print("Aimbot started!")
+    print("Press 'q' to stop aimbot")
+    while True:
+        if keyboard.is_pressed('q'):
+            print("Aimbot stopped!")
+            break
+        else:
+            pass
+else:
+    print("Aimbot stopped!")
+    exit()
+    
